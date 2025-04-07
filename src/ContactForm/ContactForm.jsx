9075db72131501e97bfb4e8928507b55da48687d
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-04-08 00:49:37 +0300

    add ContactForm

--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -1,7 +1,37 @@
+import { Field, Form, Formik } from "formik";
+import { useId } from "react";
 import s from "./ContactForm.module.css";
 
+const initialValues = {
+  name: "",
+  number: "",
+};
+
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form className={s.form}>
+        <label htmlFor={nameFieldId}>
+          Name
+          <Field type="text" name="name" id={nameFieldId}></Field>
+        </label>
+
+        <label htmlFor={numberFieldId}>
+          Number
+          <Field type="text" name="number" id={numberFieldId}></Field>
+        </label>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
