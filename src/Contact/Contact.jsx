587c91a64123daea5340de50945cba730bf12d68
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-04-07 17:46:15 +0300

    add contacts

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,7 +1,22 @@
+import { FaUser, FaPhoneAlt } from "react-icons/fa";
 import s from "./Contact.module.css";
 
-const Contact = () => {
-  return <div>Contact</div>;
+const Contact = ({ id, name, number }) => {
+  return (
+    <li className={s.contact}>
+      <div>
+        <p>
+          <FaUser />
+          <span>{name}</span>
+        </p>
+        <p>
+          <FaPhoneAlt />
+          <span>{number}</span>
+        </p>
+      </div>
+      <button>Delete</button>
+    </li>
+  );
 };
 
 export default Contact;
