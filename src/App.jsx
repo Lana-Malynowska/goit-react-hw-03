b94825d2896b8a57e75d69b47140c14f2d4c6744
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-04-09 01:49:52 +0300

    fix component bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import ContactForm from "./ContactForm/ContactForm";
-import SearchBox from "./SearchBox/SearchBox";
-import ContactList from "./ContactList/ContactList";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 
 import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
